23f7c67dd92259857399854a40edef7a5e4abac5
baoky1999 <kinataru0123@gmail.com>
2024-09-05 15:34:42 +0700

    KY update notification socket

---
+++ src/main/java/com/thi/realestateplatformsprojectbe/configs/NotificationWebSocketHandler.java
@@ -0,0 +1,38 @@
+package com.thi.realestateplatformsprojectbe.configs;
+
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationWebSocketHandler extends TextWebSocketHandler {
+
+    private final List<WebSocketSession> sessions = new ArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+    }
+
+    public void sendMessageToAll(String message) throws IOException {
+        for (WebSocketSession session : sessions) {
+            if (session.isOpen()) {
+                session.sendMessage(new TextMessage(message));
+            }
+        }
+    }
+}
---
+++ src/main/java/com/thi/realestateplatformsprojectbe/repositories/INotificationRepository.java
@@ -0,0 +1,9 @@
+package com.thi.realestateplatformsprojectbe.repositories;
+
+import com.thi.realestateplatformsprojectbe.models.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface INotificationRepository extends JpaRepository<Notification, Integer> {
+}

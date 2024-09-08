5431ea195f6fed9a2c7e2d64c105ffd69382af0f
HieuTranROM <trunghieu17082001@gmail.com>
2024-09-08 15:04:55 +0700

    admin them moi seller va buyer

---
+++ src/main/java/com/thi/realestateplatformsprojectbe/services/ICustomerService.java
@@ -0,0 +1,7 @@
+package com.thi.realestateplatformsprojectbe.services;
+
+import com.thi.realestateplatformsprojectbe.dto.CustomerDTO;
+
+public interface ICustomerService {
+    void addNewCustomer(CustomerDTO customerDTO);
+}

fe7f9520b3266d7aa5a7c6760c510f5b62a0d5b1
thien306 <nguyenvantho306@gmail.com>
2024-09-13 11:39:51 +0700

    update code

--- src/main/java/com/thi/realestateplatformsprojectbe/services/IVerificationTokenService.java
+++ src/main/java/com/thi/realestateplatformsprojectbe/services/IVerificationTokenService.java
@@ -7,4 +7,6 @@ public interface IVerificationTokenService {
     VerificationToken createVerificationToken(Account account);
     VerificationToken getVerificationToken(String token);
     void deleteToken(VerificationToken token);
+
+    VerificationToken getVerificationTokenByAccount(Account currentAccount);
 }

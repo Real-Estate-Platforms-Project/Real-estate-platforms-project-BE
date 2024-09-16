42285acd8ecd70398dc6dee6e6ed90c39001e532
levancuon <levancuongprovip@gmail.com>
2024-09-16 09:09:09 +0700

    them tinh nang tu dong vo hieu hoa tài khoang neu qua han thay doi mat khau

--- src/main/java/com/thi/realestateplatformsprojectbe/repositories/IAccountRepository.java
+++ src/main/java/com/thi/realestateplatformsprojectbe/repositories/IAccountRepository.java
@@ -3,9 +3,14 @@ import com.thi.realestateplatformsprojectbe.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface IAccountRepository extends JpaRepository<Account, Long> {
     Account findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    List<Account> findAllByExpiryDateBefore(LocalDateTime now);
 }

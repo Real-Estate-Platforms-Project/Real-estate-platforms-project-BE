f35b27e902a7827a338f13afff95feff072494f6
HieuTranROM <trunghieu17082001@gmail.com>
2024-09-17 14:46:57 +0700

    pull code moi

--- src/main/java/com/thi/realestateplatformsprojectbe/configs/SecurityConfig.java
+++ src/main/java/com/thi/realestateplatformsprojectbe/configs/SecurityConfig.java
@@ -84,6 +84,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
 //                                .requestMatchers("/api/admin/sellers/info").hasAnyRole("SELLER")
                                 .requestMatchers("/api/auth/login", "/api/auth/register", "api/auth/confirm").permitAll()
+                                .requestMatchers("/api/admin/buyers/**","/api/customers/**","/api/admin/sellers/**").hasAnyRole("ADMIN","EMPLOYEE")
                                  .requestMatchers( "/api/client/notifications").permitAll()
                                  .requestMatchers(  "/api/auth/updateForgetPassword",
                                          "/api/auth/createToken/**","/ws/notifications/**",

41325087b2aa249076e6b5675ef2f2ff4a8de391
baoky1999 <kinataru0123@gmail.com>
2024-09-17 15:00:07 +0700

    ky update tinh nang

--- src/main/java/com/thi/realestateplatformsprojectbe/configs/SecurityConfig.java
+++ src/main/java/com/thi/realestateplatformsprojectbe/configs/SecurityConfig.java
@@ -85,10 +85,9 @@ public class SecurityConfig {
 //                                .requestMatchers("/api/admin/sellers/info").hasAnyRole("SELLER")
                                 .requestMatchers("/api/auth/login", "/api/auth/register", "api/auth/confirm").permitAll()
 
-                                .requestMatchers("/api/admin/buyers/**","/api/customers/**","/api/admin/sellers/**").hasAnyRole("ADMIN","EMPLOYEE")
-                            
+                                .requestMatchers("/api/admin/buyers/**","/api/customers/**","/api/admin/sellers/**","/api/admin/notifications/**").hasAnyRole("ADMIN","EMPLOYEE")
 
-                                 .requestMatchers( "/api/client/notifications/**","/api/admin/notifications/**").permitAll()
+                                 .requestMatchers( "/api/client/notifications/**").permitAll()
 
                                  .requestMatchers(  "/api/auth/updateForgetPassword",
                                          "/api/auth/createToken/**","/ws/notifications/**",

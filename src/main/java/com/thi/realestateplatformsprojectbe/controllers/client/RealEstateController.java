f5c141c2ddc90c1858288f01ffb80dcf4f08295d
truonghoi1205 <phamtruonghoi.dmx@gmail.com>
2024-09-17 15:04:38 +0700

    hoi update notification

--- src/main/java/com/thi/realestateplatformsprojectbe/controllers/client/RealEstateController.java
+++ src/main/java/com/thi/realestateplatformsprojectbe/controllers/client/RealEstateController.java
@@ -1,8 +1,14 @@
 package com.thi.realestateplatformsprojectbe.controllers.client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thi.realestateplatformsprojectbe.dto.AccountNotificationDTO;
 import com.thi.realestateplatformsprojectbe.dto.RealEstateWithDetailDTO;
+import com.thi.realestateplatformsprojectbe.models.AccountNotification;
 import com.thi.realestateplatformsprojectbe.models.RealEstate;
+import com.thi.realestateplatformsprojectbe.services.IAccountNotificationService;
 import com.thi.realestateplatformsprojectbe.services.IRealEstateService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,47 +22,65 @@ import java.util.List;
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/api/real-estate")
-public class  RealEstateController {
+public class RealEstateController {
 
     @Autowired
     private IRealEstateService realEstateService;
 
+    @Autowired
+    private IAccountNotificationService accountNotificationService;
+
     @PostMapping
-    public ResponseEntity<RealEstate> addRealEstatePost(@RequestBody RealEstateWithDetailDTO realEstatePostDTO) {
+    public ResponseEntity<RealEstate> addRealEstatePost(@Valid @RequestBody RealEstateWithDetailDTO realEstatePostDTO) {
+
         RealEstate post = realEstateService.addRealEstatePost(realEstatePostDTO);
+        accountNotificationService.addNotification(realEstatePostDTO);
         return ResponseEntity.ok(post);
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<RealEstate> findRealEstateById(@PathVariable Long id) {
         RealEstate get = realEstateService.findById(id);
         return ResponseEntity.ok(get);
     }
-        @GetMapping("/search")
-        public ResponseEntity<Page<RealEstate>> searchRealEstates(
-                @RequestParam(required = false) String address,
-                @RequestParam(required = false) Double minPrice,
-                @RequestParam(required = false) Double maxPrice,
-                @RequestParam(required = false) List<String> location,
-                @RequestParam(required = false) List<String> demandType,
-                @RequestParam(required = false) Integer minArea,
-                @RequestParam(required = false) Integer maxArea,
-                @RequestParam(defaultValue = "0") int page,
-                @RequestParam(defaultValue = "3") int size) {
-            Pageable pageable = PageRequest.of(page, size);
-            Page<RealEstate> realEstates = realEstateService.searchRealEstates(address,minPrice, maxPrice, location, demandType, minArea, maxArea, pageable);
 
-            if (realEstates.isEmpty()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            } else {
-                return new ResponseEntity<>(realEstates, HttpStatus.OK);
-            }
+    @GetMapping("/search")
+    public ResponseEntity<Page<RealEstate>> searchRealEstates(
+            @RequestParam(required = false) String address,
+            @RequestParam(required = false) Double minPrice,
+            @RequestParam(required = false) Double maxPrice,
+            @RequestParam(required = false) List<String> location,
+            @RequestParam(required = false) List<String> demandType,
+            @RequestParam(required = false) Integer minArea,
+            @RequestParam(required = false) Integer maxArea,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "3") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<RealEstate> realEstates = realEstateService.searchRealEstates(address, minPrice, maxPrice, location, demandType, minArea, maxArea, pageable);
+
+        if (realEstates.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(realEstates, HttpStatus.OK);
         }
+    }
+
     @GetMapping("/findAll")
     public ResponseEntity<List<RealEstate>> findAllRealEstates() {
         List<RealEstate> realEstates = realEstateService.getAll();
         if (realEstates.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }else {
+        } else {
+            return new ResponseEntity<>(realEstates, HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/seller/{id}")
+    public ResponseEntity<List<RealEstate>> findAllRealEstateBySellerId(@PathVariable Long id) {
+        List<RealEstate> realEstates = realEstateService.findAllRealEstateBySellerId(id);
+        if (realEstates.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
             return new ResponseEntity<>(realEstates, HttpStatus.OK);
         }
     }
